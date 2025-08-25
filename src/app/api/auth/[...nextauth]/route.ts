86fb6049198b98c09d5089de3f3f109cb2dfd5fc
Jason <jhughes3818@gmail.com>
2025-08-25 13:24:35 +0800

    prod auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,19 +1,17 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "@/lib/db";
-
-// Providers – let’s start with GitHub (easy for dev)
 import GitHubProvider from "next-auth/providers/github";
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(db),
+  session: { strategy: "jwt" },
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
   ],
-  session: { strategy: "jwt" }, // edge-friendly
 };
 
 const handler = NextAuth(authOptions);
